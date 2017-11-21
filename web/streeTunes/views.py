60ea8488e3167eb664bbe93f263473cdb9bb0f67
William Tsao <williamtsao@Williams-MBP-2.fios-router.home>
2017-11-21 02:18:19 -0500

    TR-6 Removed login view (now handled by django.auth). Added some commets and structure

--- web/streeTunes/views.py
+++ web/streeTunes/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout, login, authenticate
+
 # Create your views here.
 def index(request):
     return render(request, 'web/index.html')
@@ -10,15 +13,30 @@ def scan(request):
 def download(request, downloadKey):
     return HttpResponse("this is download: dlkey={key}".format(key=downloadKey))
 
-def login(request):
-    return HttpResponse("this is login")
-
 def signup(request):
-    return HttpResponse("this is signup")
+    if request.method == "GET":
+        return render(request, 'web/signup.html')
+    else:
+        # Handle signup
+        return HttpResponse("You've signed up")
+        pass
+    pass
 
+
+@login_required
 def profile(request):
     return HttpResponse("this is profile")
 
+@login_required
 def dashboard(request):
     return HttpResponse("this is dashboard")
+
+@login_required
+def upload(request):
+    if request.method == "GET":
+        return render(request, 'web/upload')
+    else:
+        # Handle upload
+        pass
+    pass
 
