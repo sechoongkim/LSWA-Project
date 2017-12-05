3cbf7ec923782df3264555e77f422a92b03a1030
William Tsao <williamtsao@Williams-MBP-2.fios-router.home>
2017-12-05 12:59:34 -0500

    wip

--- web/web/settings.py
+++ web/web/settings.py
@@ -86,12 +86,13 @@ WSGI_APPLICATION = 'web.wsgi.application'
 #   }
 # }
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+exec(open(os.path.join(BASE_DIR, '..', 'db', 'db_settings.py')).read())
 
 
 # Password validation
