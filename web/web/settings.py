aaef63e741cb9368ddec608286c0f008c88aa61f
William Tsao <williamtsao@Williams-MBP-2.fios-router.home>
2017-12-06 03:57:24 -0500

    wip

--- web/web/settings.py
+++ web/web/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 INSTALLED_APPS = [
     'streeTunes.apps.StreetunesConfig',
