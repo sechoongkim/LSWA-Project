da86c1570919ba0553c003800389b5d9f10bc813
Casey <Casey@xXCeeLee.local>
2017-12-07 13:48:31 -0500

    secure redirect and os environ

--- web/web/wsgi.py
+++ web/web/wsgi.py
@@ -12,5 +12,6 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "web.settings")
+os.environ['wsgi.url_scheme'] = 'https'
 
 application = get_wsgi_application()
